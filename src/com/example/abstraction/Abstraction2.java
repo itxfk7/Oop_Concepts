2f8dd28692d780ec0db4912e302054b69ab7d943
Faraz Ahmad Khan <itxfk7@gmail.com>
2021-10-17 19:15:44 +0500

    updated abstraction2

--- src/com/example/abstraction/Abstraction2.java
+++ src/com/example/abstraction/Abstraction2.java
@@ -1,37 +1,80 @@
 package com.example.abstraction;
 
-interface A{
-    void a();
-    void b();
-    void c();
-    void d();
+interface Vehicle{
+    /**
+     * This method "vigo" is initialized in interface "Vehicle"
+     */
+    void vigo();
+    /**
+     * This method "honda" is initialized in interface "Vehicle"
+     */
+    void honda();
+    /**
+     * This method "audi" is initialized in interface "Vehicle"
+     */
+    void audi();
+    /**
+     * This method "ferrari" is initialized in interface "Vehicle"
+     */
+    void ferrari();
 }
-abstract class B implements A{
-   public void a(){
-       System.out.println("This is a");
+
+/**
+ * This abstract class "Truck" implements Vehicle interface
+ */
+abstract class Truck implements Vehicle{
+    /**
+     * Here this method "vigo" is defined and it is printing "This is a Truck"
+     */
+   public void vigo(){
+       System.out.println("This is a Truck");
 
     }
 }
-class C extends B{
-    public void b(){
-        System.out.println("This is b");
+/**
+ * This class "Cars" extends abstract class "Truck"
+ */
+class Cars extends Truck{
+    /**
+     * Here this method "honda" is defined and it is printing "This is Honda"
+     */
+    public void honda(){
+        System.out.println("This is Honda");
 
     }
-    public void c(){
-        System.out.println("This is c");
+    /**
+     * Here this method "audi" is defined and it is printing "This is audi"
+     */
+    public void audi(){
+        System.out.println("This is audi");
 
     }
-    public void d(){
-        System.out.println("This is d");
+    /**
+     * Here this method "ferrari" is defined and it is printing "This is ferrari"
+     */
+    public void ferrari(){
+        System.out.println("This is ferrari");
 
     }
 }
 public class Abstraction2 {
     public static void main(String[] args) {
-        A e=new C();
-        e.a();
-        e.b();
-        e.c();
-        e.d();
+        Vehicle object=new Cars() ;
+        /**
+         * Here, mehod vigo is called by the object we made
+         */
+        object.vigo();
+        /**
+         * Here, mehod honda is called by the object we made
+         */
+        object.honda();
+        /**
+         * Here, mehod audi is called by the object we made
+         */
+        object.audi();
+        /**
+         * Here, mehod ferrari is called by the object we made
+         */
+        object.ferrari();
     }
 }
